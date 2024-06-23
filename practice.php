42977cb946b24372b69c9563bdd523ba3f7edd1f
Some558 <167545573+Some558@users.noreply.github.com>
2024-06-23 10:53:29 +0900

    mb_substr_count

--- practice.php
+++ practice.php
@@ -1,11 +1,6 @@
 <?php
-$data = "リオとニンザブロウとナミとリンリン";
-print_r(explode("と", $data));
-    //結果:Array([0]=>リオ [1]=>ニンザブロウ [2]=>ナミ [3]=>リンリン)
-print_r(explode("や", $data));
-    //結果:Array([0]=>リオとニンザブロウとナミとリンリン)
-print_r(explode("と", $data.2));
-    //結果:Array([0]=>リオ [1]=>ニンザブロウとナミとリンリン)
-print_r(explode("と", $data, -2));
-    //結果:Array([0]=>リオ [1]=>ニンザブロウ)
+$str = "にわにはにわにわとりがいる";
+print mb_substr_count($str,"にわ"); //結果:3
+$str = "いちいちいちばにいる";
+print mb_substr_count($str,"いちいち"); //結果:1
 ?>
