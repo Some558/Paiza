52c73def5aa9775d42bdff46134c70b239f65190
Some558 <167545573+Some558@users.noreply.github.com>
2024-06-23 11:11:08 +0900

    str_ends_with

--- practice.php
+++ practice.php
@@ -1,6 +1,6 @@
 <?php
-$str = "にわにはにわにわとりがいる";
-print mb_substr_count($str,"にわ"); //結果:3
-$str = "いちいちいちばにいる";
-print mb_substr_count($str,"いちいち"); //結果:1
+$str = "WINGSプロジェクト"
+var_dump(str_contains($str, "プロ")); //結果:bool(true)
+var_dump(str_starts_with($str, "WINGS")); //結果:bool(true)
+var_dump(str_ends_with($str, "WINGS")); //結果:bool(false)
 ?>
