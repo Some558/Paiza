a6c2c0ef6878d51d3cf2e7bd887de4485cd3bef7
Some558 <167545573+Some558@users.noreply.github.com>
2024-06-24 16:31:44 +0900

    trim/ltrim/rtrim

--- practice.php
+++ practice.php
@@ -1,6 +1,7 @@
 <?php
-$str = "WINGSプロジェクト"
-var_dump(str_contains($str, "プロ")); //結果:bool(true)
-var_dump(str_starts_with($str, "WINGS")); //結果:bool(true)
-var_dump(str_ends_with($str, "WINGS")); //結果:bool(false)
+$str = "uuこんにちはu\t\n\r";
+var_dump($str); //結果:string(21) "uuこんにちはu"
+var_dump(trim($str)); //結果:string(15) "こんにちは"
+var_dump(ltrim($str)); //結果:string(19) "こんにちはu"
+var_dump(rtrim($str)); //結果:string(17) "uuこんにちは"
 ?>
