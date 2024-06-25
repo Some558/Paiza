f214d9db202ec109f1d13343c01711e81edadfe1
Some558 <167545573+Some558@users.noreply.github.com>
2024-06-25 14:33:13 +0900

    trim2

--- practice.php
+++ practice.php
@@ -1,7 +1,10 @@
 <?php
-$str = "uuこんにちはu\t\n\r";
-var_dump($str); //結果:string(21) "uuこんにちはu"
+$str = "  こんにちはu\t\n\r";
+var_dump($str); //結果:string(21) "  こんにちは "
 var_dump(trim($str)); //結果:string(15) "こんにちは"
-var_dump(ltrim($str)); //結果:string(19) "こんにちはu"
-var_dump(rtrim($str)); //結果:string(17) "uuこんにちは"
+var_dump(ltrim($str)); //結果:string(19) "こんにちは "
+var_dump(rtrim($str)); //結果:string(17) "  こんにちは"
+
+$str2 = "!===== [独習PHP] =====!";
+var_dump(trim($str2,"!=[]")); //結果:string(9) "独習PHP"
 ?>
