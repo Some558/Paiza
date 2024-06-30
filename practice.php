877cfb2c25c6b2a2c85faa1eb45bc3e579f54e0a
Some558 <167545573+Some558@users.noreply.github.com>
2024-06-30 23:25:04 +0900

    mb_send_mail

--- practice.php
+++ practice.php
@@ -1,5 +1,14 @@
-//mb_convert_encoding
+//mb_send_mail
 <?php
-file_put_contents("result.txt",
-mb_convert_encoding("こんにちは、赤ちゃん!","EUC-JP","UTF-8,SJIS,JIS"))
+$to = "wing@example.com"; //宛先
+$subject = "独習PHP 改訂版"; //件名
+$body = "こんにちは、mb_send_mail関数!\nどうですか?"; //本文
+$headers  = "From:user01@example.com\n";
+$headers  = "Cc:yamada@example.com\n";
+$headers  = "X-Mailer:PHP 8\n"; //以上、その他のヘッダー情報
+if(mb_send_mail($to,$subject,$body,$headers)){ //メール返信
+    print"メール送信に成功しました.";
+}else{
+    print"メール送信に失敗しました";
+}
 ?>
