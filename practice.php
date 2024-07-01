0dfd7bcc77d713d08a13b053ebdb05470fcbb0e4
Some558 <167545573+Some558@users.noreply.github.com>
2024-07-01 21:59:58 +0900

    count_dim

--- practice.php
+++ practice.php
@@ -1,14 +1,9 @@
-//mb_send_mail
+//count_dim
 <?php
-$to = "wing@example.com"; //宛先
-$subject = "独習PHP 改訂版"; //件名
-$body = "こんにちは、mb_send_mail関数!\nどうですか?"; //本文
-$headers  = "From:user01@example.com\n";
-$headers  = "Cc:yamada@example.com\n";
-$headers  = "X-Mailer:PHP 8\n"; //以上、その他のヘッダー情報
-if(mb_send_mail($to,$subject,$body,$headers)){ //メール返信
-    print"メール送信に成功しました.";
-}else{
-    print"メール送信に失敗しました";
-}
+$data = [
+    ["X-1","X-2","X-3"]
+    ["Y-1","Y-2","Y-3"]
+    ["Z-1","Z-2","Z-3"]
+];
+print count($data); //結果:3
 ?>
