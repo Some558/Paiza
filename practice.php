13f3619c9c35be3354a9e1f395edc2e00df3eb98
Some558 <167545573+Some558@users.noreply.github.com>
2024-07-04 02:08:57 +0900

    function

--- practice.php
+++ practice.php
@@ -1,12 +1,8 @@
-//array_merge
+//function
 <?php
-$ary1 = [1,3,5];
-$ary2 = [2,3,6];
-$result = array_merge($ary1,$ary2);
-print_r($result); //結果:Array([0]=>1 [1]=>3 [2]=>5 [3]=>2 [4]=>3 [5]=>6)
-
-$assoc1 = ["Apple"=>"Red","Orange"=>"Yellow", "Melon"=>"Green"];
-$assoc1 = ["Grape"=>"Purple","Apple"=>"Green", "Strawberry"=>"Red"];
-$result = array_merge($assoc1,$assoc2);
-print_r($result); //結果:Array([Apple]=>Green [Orange]=>Yellow) [Melon]=>Green [Grape]=>Purple [Strawberry]=>Red)
+function getTriangleArea($base,$height){
+    return $base * $height /2;
+}
+$area = getTriangleArea(8,10);
+print"三角形の面積は{$area}です。";
 ?>
