afbb563e906223edd300586f4d37c951359cf3ef
Some558 <167545573+Some558@users.noreply.github.com>
2024-07-04 23:27:20 +0900

    //function ある条件の場合はreturnで終わらせる

--- practice.php
+++ practice.php
@@ -1,8 +1,12 @@
-//function
+//function ある条件の場合はreturnで終わらせる
 <?php
 function getTriangleArea($base,$height){
-    return $base * $height /2;
-}
-$area = getTriangleArea(8,10);
-print"三角形の面積は{$area}です。";
+    //引数$base/$heightが0以下の場合は、関数を終了
+    if($base <=0 || $height<=0){
+        return;
+    }
+    return $base * $height / 2;
+    }
+$area = getTriangleArea(8,2);
+echo $area;
 ?>
