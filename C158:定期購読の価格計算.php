b983e5f6dfa80a2f7a4baa5b705ca8ad444d0453
Some558 <1641043@gmail.com>
2024-11-07 22:54:53 +0900

    回答途中

---
+++ C158:定期購読の価格計算.php
@@ -0,0 +1,18 @@
+// 1 行目で商品の種類Nと、何週間までの計算をするかのMが与えられる
+list($N, $M) = explode(' ', trim(fgets(STDIN)));
+
+// 合計金額を表す定数
+$totalCost = 0;
+
+// 最初に行数を読み取る
+$H = (int)trim(fgets(STDIN));
+
+// 商品データを格納する配列
+$items = [];
+
+// H行のデータを読み取る
+for($i = 0; $i<H; $i++){
+	f_iとp_iを分ける
+	list($f, $p) = explode(' ', trim(fgets(STDIN)))
+	$items[] 0
+	明日はここから進める
