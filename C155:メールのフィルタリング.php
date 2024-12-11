ced19ca51a0f14a70ed8f1dc5e9868d22635ab68
Some558 <1641043@gmail.com>
2024-12-11 22:58:41 +0900

    STDIN変更

--- C155:メールのフィルタリング.php
+++ C155:メールのフィルタリング.php
@@ -1,13 +1,13 @@
 <?php
     // 入力の取得
-    $S = trim(fgets($stdin)); // 広告と判定する語
-    $N = intval(trim(fgets($stdin))); // メール件数
+    $S = trim(fgets(STDIN)); // 広告と判定する語
+    $N = intval(trim(fgets(STDIN))); // メール件数
 
     $results = [];
 
     // メールタイトルを読み込み
     for ($i = 0; $i < $N; $i++) {
-        $title = trim(fgets($stdin));
+        $title = trim(fgets(STDIN));
         if (strpos($title, $S) !== false) {
             $results[] = "Yes";
         } else {
