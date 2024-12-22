6347b6471180e3fc788af9e397e7d5791d1e7f92
Some558 <1641043@gmail.com>
2024-12-23 08:05:16 +0900

    コメント編集

--- C157:花のブーケ.php
+++ C157:花のブーケ.php
@@ -5,7 +5,7 @@ $N = (int)trim(fgets(STDIN));
 // 花の種類を標準入力から取得し、配列に変換
 $flowerKinds = explode(' ', trim(fgets(STDIN)));
 
-// 花の種類を一意にするために配列をセットに変換
+// 花の種類を一意にするために配列をセットに変換する
 $uniqueFlowerKinds = array_unique($flowerKinds);
 
 // 結果としてユニークな花の種類の数を出力
